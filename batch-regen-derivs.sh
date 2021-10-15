e1a63cc4c62b1b42b7995f411717e4915e3756fc
Bryan J. Brown <bjbrown@fsu.edu>
2021-10-15 12:24:48 -0500

    Turn off debug mode

--- batch-regen-derivs.sh
+++ batch-regen-derivs.sh
@@ -20,8 +20,7 @@ else
     TIME=$(date)
     PADDED_ITERATOR=$(printf "%04d\n" $ITERATOR)
     echo "$TIME: Processing $LINE (page $PADDED_ITERATOR of $PADDED_PID_COUNT)..."
-    DRUSH_COMMAND="drush -u 1 --root=/var/www/html idr --force $DSID_ARG --pids=$LINE"
-    echo $DRUSH_COMMAND
+    drush -u 1 --root=/var/www/html idr --force $DSID_ARG --pids=$LINE
     ITERATOR=$(expr $ITERATOR + 1)
   done < $PID_FILE > $PID_FILE.log 2>&1
 fi
